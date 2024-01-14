1136a07b7fbca5ad85f28e42472ba91ca962038e
Monutchuan <Monutchuan@gmail.com>
2024-01-14 09:29:40 +0800

    Import Project Base.

---
+++ Source/Random/RandomGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RandomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RANDOM_API ARandomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
